29ce495335b39b27e9e0243a412fed28b1828083
juanudk <juanudkp@gmail.com>
2021-07-17 20:21:00 -0300

    Fix upload error

--- lesson2/upload.php
+++ lesson2/upload.php
@@ -1,17 +1,11 @@
 <?php
-$target_dir = "C:\Estudos\ExamplesWeb\lesson2\uploads\uploads";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+error_reporting(E_ALL);
+$target_dir = ".\uploads\\";
+$uploadfile  = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
+  echo "File uploaded with success\n";
+} else {
+  echo "Error uploading the file!\n";
 }
 ?>
